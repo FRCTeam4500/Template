bf5a3f84958ab7863e6ee509d868e65f7a9a0194
Vimal Buckley <vbuckley68@claytonschools.net>
2024-01-06 18:05:40 -0600

    Fixed new TalonFX

--- src/main/java/frc/robot/hardware/TalonFXMotorController.java
+++ src/main/java/frc/robot/hardware/TalonFXMotorController.java
@@ -1,53 +1,56 @@
 package frc.robot.hardware;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
+import com.ctre.phoenix6.controls.VelocityDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.pathplanner.lib.util.PIDConstants;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class TalonFXMotorController extends TalonFX implements EncodedMotorController {
-    private double TICKS_PER_RADIAN = 2048 / Math.PI / 2;
+    // private double TICKS_PER_RADIAN = 2048 / Math.PI / 2;
+    private TalonFXConfiguration config;
 
     public TalonFXMotorController(int deviceID) {
         super(deviceID);
-        config_IntegralZone(0, 0);
-        configMotionCruiseVelocity(10000);
-        configMotionAcceleration(10000);
-        configAllowableClosedloopError(0, 0);
-        configClearPositionOnQuadIdx(true, 10);
+        config = new TalonFXConfiguration();
+        getConfigurator().apply(config);
+        config.MotionMagic.MotionMagicAcceleration = 9999;
+        config.MotionMagic.MotionMagicCruiseVelocity = 9999;
     }
 
     @Override
     public void setOutput(double output) {
-        set(ControlMode.PercentOutput, output);
+        setControl(new DutyCycleOut(output));
     }
 
     @Override
     public double getOutput() {
-        return getMotorOutputPercent();
+        return get();
     }
 
     @Override
     public void setAngularVelocity(Rotation2d velocity) {
-        set(ControlMode.Velocity, velocity.getRadians() * TICKS_PER_RADIAN / 10.0);
+        setControl(new VelocityDutyCycle(velocity.getRotations()));
     }
 
     @Override
     public Rotation2d getAngularVelocity() {
-        return Rotation2d.fromRadians(getSelectedSensorVelocity() / TICKS_PER_RADIAN * 10);
+        return Rotation2d.fromRotations(getRotorVelocity().getValueAsDouble());
     }
 
     @Override
     public void setAngle(Rotation2d angle) {
-        set(ControlMode.MotionMagic, angle.getRadians() * TICKS_PER_RADIAN);
+        setControl(new MotionMagicDutyCycle(angle.getRotations()));
     }
 
     @Override
     public Rotation2d getAngle() {
-        return Rotation2d.fromRadians(getSelectedSensorPosition() / TICKS_PER_RADIAN);
+        return Rotation2d.fromRotations(getRotorPosition().getValueAsDouble());
     }
 
     @Override
@@ -57,65 +60,67 @@ public class TalonFXMotorController extends TalonFX implements EncodedMotorContr
 
     @Override
     public TalonFXMotorController configCurrentLimit(int currentLimit) {
-        configSupplyCurrentLimit(
-            new SupplyCurrentLimitConfiguration(
-                true, 
-                currentLimit, 
-                currentLimit + 1, 
-                0.1
-            ), 
-            50
-        );
+        config.CurrentLimits.SupplyCurrentLimitEnable = true;
+        config.CurrentLimits.SupplyCurrentLimit = currentLimit;
+        config.CurrentLimits.SupplyCurrentThreshold = currentLimit+1;
+        config.CurrentLimits.SupplyTimeThreshold = 0.1;
         return this;
     }
 
     @Override
     public TalonFXMotorController configPID(PIDConstants pid) {
-        config_kP(0, pid.kP);
-        config_kI(0, pid.kI);
-        config_kD(0, pid.kD);
+        config.Slot0.kP = pid.kP;
+        config.Slot0.kI =  pid.kI;
+        config.Slot0.kD = pid.kD;
         return this;
     }
 
     @Override
     public TalonFXMotorController configMinAngle(Rotation2d min) {
-        configReverseSoftLimitEnable(true);
-        configReverseSoftLimitThreshold(min.getRadians() * TICKS_PER_RADIAN);
+        config.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+        config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = min.getRotations();
         return this;
     }
 
     @Override
     public TalonFXMotorController configMaxAngle(Rotation2d max) {
-        configForwardSoftLimitEnable(true);
-        configForwardSoftLimitThreshold(max.getRadians() * TICKS_PER_RADIAN);
+        config.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+        config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = max.getRotations();
         return this;
     }
 
     @Override
     public TalonFXMotorController configMinOutput(double minOutput) {
-        configPeakOutputReverse(minOutput);
+        config.MotorOutput.PeakReverseDutyCycle = minOutput;
        return this;
     }
 
     @Override
     public TalonFXMotorController configMaxOutput(double maxOutput) {
-        configPeakOutputForward(maxOutput);
+        config.MotorOutput.PeakForwardDutyCycle = maxOutput;
         return this;
     }
     
     @Override
     public TalonFXMotorController configInverted(boolean shouldInvert) {
-        setInverted(shouldInvert);
+        getConfigurator().refresh(config);
+        if (shouldInvert) {
+            if (config.MotorOutput.Inverted == InvertedValue.Clockwise_Positive) {
+                config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+            } else {
+                config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+            }
+        }
         return this;
     }
 
     @Override
     public TalonFXMotorController configBrakeOnIdle(boolean shouldBreak) {
-        setNeutralMode(
-            shouldBreak
-            ? NeutralMode.Brake
-            : NeutralMode.Coast
-        );
+        if (shouldBreak) {
+            config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        } else {
+            config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        }
         return this;
     }
 }
