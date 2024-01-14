3e2f1d585d6ac17bc5960ed43a72c2969d3b615a
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2024-01-13 22:25:23 -0600

    Swtiched to using WPI's discritize

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
