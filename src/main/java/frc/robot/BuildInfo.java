937a81f25c11d5f6b1e01dd3acd953a9cd5400de
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2024-01-17 21:18:26 -0600

    Add gradle stuff to update BuildInfo

--- src/main/java/frc/robot/BuildInfo.java
+++ src/main/java/frc/robot/BuildInfo.java
@@ -4,16 +4,16 @@ package frc.robot;
  * Automatically generated file containing build version information.
  */
 public final class BuildInfo {
-    public static final String MAVEN_GROUP = "";
-    public static final String MAVEN_NAME = "Template";
-    public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 57;
-    public static final String GIT_SHA = "7b2e721105524296e9929cf9a8a71239d577143a";
-    public static final String GIT_DATE = "2023-12-19 09:18:58 CST";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2023-12-28 21:04:34 CST";
-    public static final long BUILD_UNIX_TIME = 1703819074648L;
-    public static final int DIRTY = 1;
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Template";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 82;
+  public static final String GIT_SHA = "bbdf382f933272cc1716fba5ed016161b5a179f4";
+  public static final String GIT_DATE = "2024-01-15 12:35:19 EST";
+  public static final String GIT_BRANCH = "2024WorkingSwerve";
+  public static final String BUILD_DATE = "2024-01-17 22:18:04 EST";
+  public static final long BUILD_UNIX_TIME = 1705547884638L;
+  public static final int DIRTY = 1;
 
-    private BuildInfo(){}
+  private BuildInfo(){}
 }
