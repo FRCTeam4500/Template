6ad2d65ddb044ee5e685e7af1d2da2fc608fece6
Vimal Buckley <vbuckley68@claytonschools.net>
2024-01-21 18:54:25 -0600

    Updated WPI to 2024.2.1
    Doesn't build yet due to AKit not having an update out yet.

--- src/main/java/frc/robot/BuildInfo.java
+++ src/main/java/frc/robot/BuildInfo.java
@@ -7,13 +7,13 @@ public final class BuildInfo {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 82;
-  public static final String GIT_SHA = "bbdf382f933272cc1716fba5ed016161b5a179f4";
-  public static final String GIT_DATE = "2024-01-15 12:35:19 EST";
+  public static final int GIT_REVISION = 83;
+  public static final String GIT_SHA = "937a81f25c11d5f6b1e01dd3acd953a9cd5400de";
+  public static final String GIT_DATE = "2024-01-17 22:18:26 EST";
   public static final String GIT_BRANCH = "2024WorkingSwerve";
-  public static final String BUILD_DATE = "2024-01-17 22:18:04 EST";
-  public static final long BUILD_UNIX_TIME = 1705547884638L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-01-21 19:53:35 EST";
+  public static final long BUILD_UNIX_TIME = 1705884815314L;
+  public static final int DIRTY = 0;
 
   private BuildInfo(){}
 }
