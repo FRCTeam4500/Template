01f70c151781da57fd6096199700186af35b1d03
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2024-01-22 19:05:03 -0600

    Updated Vendor Deps

--- src/main/java/frc/robot/BuildInfo.java
+++ src/main/java/frc/robot/BuildInfo.java
@@ -7,13 +7,13 @@ public final class BuildInfo {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 83;
-  public static final String GIT_SHA = "937a81f25c11d5f6b1e01dd3acd953a9cd5400de";
-  public static final String GIT_DATE = "2024-01-17 22:18:26 EST";
+  public static final int GIT_REVISION = 84;
+  public static final String GIT_SHA = "6ad2d65ddb044ee5e685e7af1d2da2fc608fece6";
+  public static final String GIT_DATE = "2024-01-21 19:54:25 EST";
   public static final String GIT_BRANCH = "2024WorkingSwerve";
-  public static final String BUILD_DATE = "2024-01-21 19:53:35 EST";
-  public static final long BUILD_UNIX_TIME = 1705884815314L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2024-01-22 20:04:36 EST";
+  public static final long BUILD_UNIX_TIME = 1705971876792L;
+  public static final int DIRTY = 1;
 
   private BuildInfo(){}
 }
