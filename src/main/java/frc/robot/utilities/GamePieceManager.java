d9bdd0f3eaefc3824f7364ca30940b33754465e4
Vimal Buckley <vbuckley68@claytonschools.net>
2024-10-13 17:39:01 -0500

    Edit GamePiece Manager

--- src/main/java/frc/robot/utilities/GamePieceManager.java
+++ src/main/java/frc/robot/utilities/GamePieceManager.java
@@ -1,33 +1,35 @@
 package frc.robot.utilities;
 
 import java.util.Set;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class GamePieceManager {
-
+    private static HashMap<NetworkTable, Pose3d> cameras = new HashMap<>();
     private static Set<Translation2d> pieces = new HashSet<>();
     public static void resetField() {
         pieces.clear();
-        addPiece(new Translation2d(2.9, 7));
-        addPiece(new Translation2d(2.9, 5.55));
-        addPiece(new Translation2d(2.9, 4.1));
-        addPiece(new Translation2d(8.3, 7.44));
-        addPiece(new Translation2d(8.3, 5.78));
-        addPiece(new Translation2d(8.3, 4.11));
-        addPiece(new Translation2d(8.3, 2.44));
-        addPiece(new Translation2d(8.3, 0.77));
-        addPiece(new Translation2d(13.67, 7));
-        addPiece(new Translation2d(13.67, 5.55));
-        addPiece(new Translation2d(13.67, 4.1));
+        pieces.add(new Translation2d(2.9, 7));
+        pieces.add(new Translation2d(2.9, 5.55));
+        pieces.add(new Translation2d(2.9, 4.1));
+        pieces.add(new Translation2d(8.3, 7.44));
+        pieces.add(new Translation2d(8.3, 5.78));
+        pieces.add(new Translation2d(8.3, 4.11));
+        pieces.add(new Translation2d(8.3, 2.44));
+        pieces.add(new Translation2d(8.3, 0.77));
+        pieces.add(new Translation2d(13.67, 7));
+        pieces.add(new Translation2d(13.67, 5.55));
+        pieces.add(new Translation2d(13.67, 4.1));
         log();
     }
 
@@ -41,6 +43,10 @@ public class GamePieceManager {
         log();
     }
 
+    public static void addCamera(String name, Pose3d offset) {
+        cameras.put(NetworkTableInstance.getDefault().getTable(name), offset);
+    }
+
     private static void log() {
         Translation2d[] array = new Translation2d[pieces.size()];
         int i = 0;
@@ -50,55 +56,44 @@ public class GamePieceManager {
         }
         DogLog.log("Pieces", array);
     }
-
-    public static void updateLimelightNT(
-        NetworkTable table, 
-        Pose2d robotPose, 
-        Pose3d cameraOffsets
-    ) {
-        Rotation3d cameraRotation3d = new Rotation3d(
-            cameraOffsets.getRotation().getX(), 
-            cameraOffsets.getRotation().getY(), 
-            robotPose.getRotation().getRadians() + cameraOffsets.getRotation().toRotation2d().getRadians()
-        );
-        Translation3d cameraTranslation3d = new Translation3d(
-            robotPose.getX() + cameraOffsets.getX(), 
-            robotPose.getY() + cameraOffsets.getY(), 
-            cameraOffsets.getZ()
-        );
-        Pose3d camera = new Pose3d(cameraTranslation3d, cameraRotation3d);
-        Pose3d seenPiece = null;
-        for (Translation2d piece : pieces) {
-            Pose3d poseVer = new Pose3d(new Pose2d(piece, new Rotation2d()));
-            Pose3d thisPiece = poseVer.relativeTo(camera);
-            double distance = thisPiece.getTranslation().getNorm();
-            if (seenPiece == null) {
-                double up = Math.abs(thisPiece.getZ());
-                double sideways = Math.abs(thisPiece.getY());
-                double upAngle = Math.toDegrees(Math.asin(up/distance));
-                double sideAngle = Math.toDegrees(Math.asin(sideways/distance));
-                if (upAngle < 25 && sideAngle < 30) {
-                    seenPiece = thisPiece;
-                } 
-            } else {
-                double thisDistance = thisPiece.getTranslation().toTranslation2d().getNorm();
-                double seenDistance = seenPiece.getTranslation().toTranslation2d().getNorm();
-                if (thisDistance < seenDistance) {
-                    seenPiece = thisPiece;
+    
+    public static void updateNT(Pose2d robotPose) {
+        for (Map.Entry<NetworkTable, Pose3d> cameraEntry: cameras.entrySet()) {
+            Pose3d camera = new Pose3d(robotPose).plus(new Transform3d(new Pose3d(), cameraEntry.getValue()));
+            boolean seenPiece = false;
+            double upAngle = 0;
+            double sideAngle = 0;
+            for (Translation2d piece : pieces) {
+                Pose3d poseVer = new Pose3d(new Pose2d(piece, new Rotation2d()));
+                Pose3d thisPiece = poseVer.relativeTo(camera);
+                double thisDist = thisPiece.getTranslation().getNorm();
+                double thisUp = thisPiece.getZ();
+                double thisSide = thisPiece.getY();
+                double thisUpAngle = Math.toDegrees(Math.asin(thisUp/thisDist));
+                double thisSideAngle = Math.toDegrees(Math.asin(thisSide/thisDist));
+                if (seenPiece) {
+                    double thisCenterOffset = Math.hypot(thisSideAngle, thisUpAngle);
+                    double seenCenterOffset = Math.hypot(sideAngle, upAngle);
+                    if (thisCenterOffset < seenCenterOffset) {
+                        upAngle = thisUpAngle;
+                        sideAngle = thisSideAngle;
+                    }
+                } else {
+                    if (Math.abs(thisUpAngle) < 25 && Math.abs(thisSideAngle) < 30) {
+                        seenPiece = true;
+                        upAngle = thisUpAngle;
+                        sideAngle = thisSideAngle;
+                    }
                 }
             }
-        }
-        if (seenPiece == null) {
-            table.getEntry("tv").setInteger(0);
-        } else {
-            double distance = seenPiece.getTranslation().getNorm();
-            double up = seenPiece.getZ();
-            double sideways = seenPiece.getY();
-            double upAngle = Math.toDegrees(Math.asin(up/distance));
-            double sideAngle = Math.toDegrees(Math.asin(sideways/distance));
-            table.getEntry("tv").setInteger(1);
+            NetworkTable table = cameraEntry.getKey(); 
+            if (seenPiece) {
+                table.getEntry("tv").setInteger(1);
+            } else {
+                table.getEntry("tv").setInteger(0);
+            }
             table.getEntry("tx").setNumber(-sideAngle);
             table.getEntry("ty").setNumber(upAngle);
-        }       
+        }
     }
 }
