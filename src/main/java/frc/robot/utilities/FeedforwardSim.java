0f56c73346f99cd76d67981a08fb19b3219fbb41
Nate Brown <nravinbrown@gmail.com>
2024-12-12 13:17:36 -0600

    documented some utilities

--- src/main/java/frc/robot/utilities/FeedforwardSim.java
+++ src/main/java/frc/robot/utilities/FeedforwardSim.java
@@ -7,20 +7,26 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FeedforwardSim extends SubsystemBase {
+    /** function that modifies the State argument, stepping forward in time by 20 ms */
     private BiConsumer<State, Double> calc;
+    /** position and velocity */
     private State state;
+    /** current voltage */
     private double volts;
 
     /**
-     * @param calc A function that takes the current state and the voltage, and
-     * modifies the passed in state to be the state after 0.02 seconds
+     * @param calc A function that steps the state forward in time (by 20 ms)
      * @param initalState The inital state of the mechanism
+     * @apiNote calc should modify its arguments
      */
     public FeedforwardSim(BiConsumer<State, Double> calc, State initalState) {
         this.calc = calc;
         this.state = initalState;
     }
 
+    /**
+     * If this Simulator is enabled, steps time forward by 20 ms.
+     */
     public void periodic() {
         if (DriverStation.isDisabled()) {
             volts = 0;
@@ -28,22 +34,29 @@ public class FeedforwardSim extends SubsystemBase {
         calc.accept(state, volts);
     }
 
+    /** setter function
+     * @param volts new voltage
+     */
     public void setVoltage(double volts) {
         this.volts = volts;
     }
 
+    /** voltage getter */
     public double getVoltage() {
         return volts;
     }
 
+    /** position getter */
     public double getPosition() {
         return state.position;
     }
 
+    /** velocity getter */
     public double getVelocity() {
         return state.velocity;
     }
 
+    /** changes position */
     public void resetPosition(double newPosition) {
         state.position = newPosition;
     }
@@ -58,19 +71,10 @@ public class FeedforwardSim extends SubsystemBase {
      * @param kV The voltage needed to cause a given constant velocity.
      * @param kA The voltage needed to cause a given acceleration
      * @param initialState The inital position and velocity of the mechanism
+     * @implNote this is just {@link frc.robot.utilities.FeedforwardSim#createElevator} but where gravity is 0.
      */
-    public static FeedforwardSim createFlywheel(double kS, double kV, double kA, State initalState) {
-        return new FeedforwardSim(
-            (state, voltage) -> {
-                double staticVolts = Math.signum(state.velocity) * kS;
-                double velocityVolts = state.velocity * kV;
-                double deltaVel = 0.02 * (voltage - staticVolts - velocityVolts) / kA;
-                double averageVel = state.velocity + deltaVel / 2;
-                state.position += 0.02 * averageVel;
-                state.velocity += deltaVel;
-                
-            }, initalState
-        );
+    public static FeedforwardSim createFlywheel(double kS, double kV, double kA, State initialState) {
+        return createElevator(0, kS, kV, kA, initialState);
     }
 
     /**
